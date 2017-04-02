48c18fa3d4c940b711c675fadad02aa0e2b22837
Stefano Corallo <stefanorg@gmail.com>
2017-04-02 17:58:36 +0200

    initial commit

---
+++ src/main/application/build/temp/development/HelloWorld/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/n4z4/lavoro/j2ee-dev-env/src/main/application/build/.sass-cache'
---
+++ src/main/application/build/temp/development/HelloWorld/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/n4z4/lavoro/j2ee-dev-env/src/main/application/app.js
