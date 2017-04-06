02989be5732160c81c600699ab82dbe2bf847b3d
Stefano Corallo <stefanorg@gmail.com>
2017-04-06 12:37:41 +0200

    - fix jpa - aggiunta configurazione per datasource - abilitato debug di default

---
+++ src/main/java/it/helloworld/entity/User.java
@@ -0,0 +1,11 @@
+package it.helloworld.entity;
+
+
+/**
+ * Created by stefano on 05/04/17.
+ */
+
+public class User {
+
+
+}
--- src/main/java/it/helloworld/rest/HelloWorld.java
+++ src/main/java/it/helloworld/rest/HelloWorld.java
@@ -1,8 +1,12 @@
 package it.helloworld.rest;
 
+import org.json.simple.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import java.util.HashMap;
 
 /**
  * Created by n4z4 on 04/04/17.
@@ -14,7 +18,18 @@ public class HelloWorld {
     @GET
     @Path("/world")
     public String sayWorld() {
-        return "{ \"Hello\": \"World\" }";
+        JSONObject res = new JSONObject();
+        res.put("Hello", "World");
+        return res.toJSONString();
+    }
+
+    @GET
+    @Path("/echo/{word}")
+    public String echo(@PathParam("word") String word) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("You said", word);
+        JSONObject res = new JSONObject(map);
+        return res.toJSONString();
     }
 
 }
